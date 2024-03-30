d084e7852c49fd60519d389de5677529de61c514
mpedroni <mpedroni77@gmail.com>
2024-03-30 00:24:40 -0300

    first commit :tada:

---
+++ src/main/java/com/mpedroni/bytebookstore/ByteBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mpedroni.bytebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteBookstoreApplication.class, args);
+    }
+
+}
